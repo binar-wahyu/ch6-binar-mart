2b134cf4e92393b0bd72c6251de079870c53486a
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-05-17 13:00:36 +0800

    Complete UI

--- router/index.js
+++ router/index.js
@@ -2,11 +2,33 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("pages/products/index");
+  res.render("pages/home/index");
+});
+
+router.get("/products", (req, res) => {
+  res.render("pages/products/index", { pageTitle: "Daftar Barang" });
+});
+
+router.get("/products/create", (req, res) => {
+  res.render("pages/products/create", { pageTitle: "Buat Barang" });
+});
+
+router.get("/products/:id", (req, res) => {
+  res.render("pages/products/show", { pageTitle: "Barang: Masker Medis" });
 });
 
 router.get("/suppliers", (req, res) => {
-  res.render("pages/suppliers/index");
+  res.render("pages/suppliers/index", { pageTitle: "Daftar Supplier" });
+});
+
+router.get("/suppliers/create", (req, res) => {
+  res.render("pages/suppliers/create", { pageTitle: "Buat Supplier" });
+});
+
+router.get("/suppliers/:id", (req, res) => {
+  res.render("pages/suppliers/show", {
+    pageTitle: "Supplier: PT. Mitra Utama",
+  });
 });
 
 module.exports = router;
