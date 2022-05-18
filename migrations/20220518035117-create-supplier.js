1c48551ff39dd8a0a1f14be7d59bf23650d646ca
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-05-18 11:59:00 +0800

    Setup supplier model and migration

---
+++ migrations/20220518035117-create-supplier.js
@@ -0,0 +1,41 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Suppliers", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      phoneNumber: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.TEXT,
+      },
+      joinDate: {
+        type: Sequelize.DATEONLY,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Suppliers");
+  },
+};
