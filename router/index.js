8701e289025885f44a2a4b7e1c138c7acad46bc8
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-05-18 12:27:42 +0800

    Implement create and read Supplier

--- router/index.js
+++ router/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { Supplier } = require("../models");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -18,11 +20,39 @@ router.get("/products/:id", (req, res) => {
 });
 
 router.get("/suppliers", (req, res) => {
-  res.render("pages/suppliers/index", { pageTitle: "Daftar Supplier" });
+  Supplier.findAll({
+    order: [["name", "ASC"]],
+  }).then((suppliers) => {
+    res.render("pages/suppliers/index", {
+      pageTitle: "Daftar Supplier",
+      suppliers,
+    });
+  });
 });
 
 router.get("/suppliers/create", (req, res) => {
   res.render("pages/suppliers/create", { pageTitle: "Buat Supplier" });
+});
+
+router.post("/suppliers", (req, res) => {
+  // Database tidak dapat menerima string kosong dalam memasukkan date
+  // Jadi harus dilakukan pengecekan untuk konversi string kosong jadi null
+  let joinDate;
+  if (!req.body.joinDate) {
+    joinDate = null;
+  } else {
+    joinDate = req.body.joinDate;
+  }
+
+  Supplier.create({
+    name: req.body.name,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    address: req.body.address,
+    joinDate,
+  }).then(() => {
+    res.redirect("/suppliers");
+  });
 });
 
 router.get("/suppliers/:id", (req, res) => {
